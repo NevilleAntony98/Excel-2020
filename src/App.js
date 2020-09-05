8b99a6d8fecaad2959a339aa21c61044e24cf51c
Nikhil713 <nikhilbabu213@gmail.com>
2020-09-05 12:12:06 +0530

    remove build error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+// import {Switch, Route} from 'react-router-dom';
 import './App.scss';
 
 export default class App extends React.Component {
@@ -8,15 +8,15 @@ export default class App extends React.Component {
           <div>
               MAIN
               {/* <Nav /> */}
-              <Switch>
-                  {/* <Route path="/home" component={Home} />
+              {/* <Switch>
+                  <Route path="/home" component={Home} />
                   <Route path="/competitions" component={Competitions} />
                   <Route path="/events" component={Events} />
                   <Route path="/schedule" component={Schedule} />
                   <Route path="/contacts" component={ContactsPage} />
-                  <Route exact path="/" component={Home} /> */}
-              </Switch>
+                  <Route exact path="/" component={Home} />
+              </Switch> */}
           </div>
       )
   }
-}
+}   
