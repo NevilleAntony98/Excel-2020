0dd4fb8e86e594c489e3f6339fd204b37d516363
Neville <nevilleantony98@gmail.com>
2020-10-01 20:59:13 +0530

    shared-components: Introduce header-bar

---
+++ src/shared-components/header-bar/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './index.scss'
+
+export default function HeaderBar (props) {
+    if (!props.title)
+        return null
+
+    return (
+        <div className="competitions-header">
+            <a className="title" href="/">{props.title}</a>
+            {props.subtitle && <span className="subtitle">{props.subtitle}</span>}
+        </div>
+    )
+}
