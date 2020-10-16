46fa6e1547485b175b97c01a8224ec3962d9a9e8
Rohit <rohitmpaul@gmail.com>
2020-10-16 18:13:20 +0530

    close sidebar on clicking outside it

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import "./Sidebar.css";
 
 const Sidebar = ({ setIsSidebarOpen }) => {
+    
+  const useOutsideAlerter = (ref) => {
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setIsSidebarOpen(false);
+        }
+      }
+
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  };
+
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef);
+
   return (
-    <div className="sidebar-container">
+    <div className="sidebar-container" ref={wrapperRef}>
       <div className="sidebar-pic">Pic</div>
-      <div className="sidebar-row" onClick={() => setIsSidebarOpen(false)}>Hello</div>
-      <div className="sidebar-row" onClick={() => setIsSidebarOpen(false)}>Hi</div>
+      <div className="sidebar-row" onClick={() => setIsSidebarOpen(false)}>
+        Hello
+      </div>
+      <div className="sidebar-row" onClick={() => setIsSidebarOpen(false)}>
+        Hi
+      </div>
     </div>
   );
 };
