b925a5355d102aae4695873f57e2aebeb2710dbd
Rohit <rohitmpaul@gmail.com>
2020-11-14 19:18:41 +0530

    add icons in sidebar

--- src/components/Navbar/Sidebar.js
+++ src/components/Navbar/Sidebar.js
@@ -1,5 +1,14 @@
 import React, { useEffect, useRef } from "react";
 
+import home from "../../assets/svg/home.svg";
+import schedule from "../../assets/svg/schedule.svg";
+import competitions from "../../assets/svg/competitions.svg";
+import contacts from "../../assets/svg/contacts.svg";
+import events from "../../assets/svg/events.svg";
+import facebook from "../../assets/svg/facebook.svg";
+import instagram from "../../assets/svg/instagram.svg";
+import twitter from "../../assets/svg/twitter.svg";
+
 import "./Sidebar.css";
 
 const Sidebar = ({ setIsSidebarOpen }) => {
@@ -25,28 +34,29 @@ const Sidebar = ({ setIsSidebarOpen }) => {
     <div className="sidebar-container" ref={wrapperRef}>
       <div className="sidebar-pic">Pic</div>
       <div className="sidebar-row" onClick={() => setIsSidebarOpen(false)}>
+        <img alt="home" src={home} className="sidebarIcon" />
         Home
       </div>
       <div className="sidebar-row" onClick={() => setIsSidebarOpen(false)}>
+        <img alt="schedule" src={schedule} className="sidebarIcon" />
         Schedule
       </div>
       <div className="sidebar-row" onClick={() => setIsSidebarOpen(false)}>
+        <img alt="competitions" src={competitions} className="sidebarIcon" />
         Competitions
       </div>
       <div className="sidebar-row" onClick={() => setIsSidebarOpen(false)}>
+        <img alt="events" src={events} className="sidebarIcon" />
         Events
       </div>
       <div className="sidebar-row" onClick={() => setIsSidebarOpen(false)}>
+        <img alt="contacts" src={contacts} className="sidebarIcon" />
         Contacts
       </div>
-      <div className="sidebar-row" onClick={() => setIsSidebarOpen(false)}>
-        Facebook
-      </div>
-      <div className="sidebar-row" onClick={() => setIsSidebarOpen(false)}>
-        Twitter
-      </div>
-      <div className="sidebar-row" onClick={() => setIsSidebarOpen(false)}>
-        Github
+      <div className="sidebar-rowSocial">
+        <img alt="facebook" src={facebook} className="sidebarSocialIcon" />
+        <img alt="instagram" src={instagram} className="sidebarSocialIcon" />
+        <img alt="twitter" src={twitter} className="sidebarSocialIcon" />
       </div>
     </div>
   );
