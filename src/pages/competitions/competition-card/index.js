10ecf430cb0e3b59cf8420ea159dd713749fe483
Neville <nevilleantony98@gmail.com>
2020-11-17 17:15:39 +0530

    competition-card: Do animations

--- src/pages/competitions/competition-card/index.js
+++ src/pages/competitions/competition-card/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Spring, config } from 'react-spring/renderprops';
 
 import './index.scss'
 
+const excelIcon = require('../../../data/images/excel2020.png')
+
 export default class CompetitionCard extends React.Component {
+    state = {
+        imageIsReady: false
+    }
+
     onRegisterClicked = () => {
         console.log("Register clicked for " + this.props.competition.name);
     }
@@ -22,17 +29,60 @@ export default class CompetitionCard extends React.Component {
         )
     }
 
+    componentDidMount() {
+        const img = new Image()
+        img.src = this.props.competition.icon
+
+        img.onload = () => {
+            this.setState({ imageIsReady: true });
+          }
+    }
+
+    PlaceholderIcon = () => {
+        return(
+            <img src={excelIcon} alt={this.props.competition.name}/>
+        )
+    }
+
+    CompetitionIcon = () => {
+        return(
+            <Spring
+            from={{ transform: 'rotateY(360deg)' }}
+            to={{ transform: 'rotateY(0deg)' }}
+            config={{ tension: 200, friction: 40 }}>
+                {props => (
+                    <div style={props}>
+                        <img src={this.props.competition.icon} alt={this.props.competition.name}/>
+                    </div>
+                )}
+            </Spring>
+        )
+    }
+
     render() {
+
         return (
-            <div className="competition-card">
-                <div className="icon">
-                    <img src={this.props.competition.icon} alt={this.props.competition.name}/>
-                </div>
-                <div className="title">
-                    <span>{this.props.competition.name}</span>
-                </div>
-                <this.registerElement />
-            </div>
+            <Spring
+            from={{ opacity: 0, marginTop: -50}}
+            to={{ opacity: 1, marginTop: 0 }}
+            config={config.wobbly}
+            >
+                {props => (
+                    <div style={props}>
+                        <div className="competition-card">
+                            <div className="icon">
+                                {!this.state.imageIsReady ?
+                                <this.PlaceholderIcon /> :
+                                <this.CompetitionIcon />}
+                            </div>
+                            <div className="title">
+                                <span>{this.props.competition.name}</span>
+                            </div>
+                            <this.registerElement />
+                        </div>
+                    </div>
+                )}
+            </Spring>
         );
     }
 }
