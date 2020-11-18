76a8b0558df4ed751df2aecb2dd75330a2095a23
Rohit <rohitmpaul@gmail.com>
2020-11-18 12:27:32 +0530

    add the hectagon on bottom navbar

--- src/components/Navbar/Bottombar.js
+++ src/components/Navbar/Bottombar.js
@@ -7,19 +7,19 @@ import schedule from '../../assets/svg/schedule.svg';
 import competitions from '../../assets/svg/competitions.svg';
 import contacts from '../../assets/svg/contacts.svg';
 import events from '../../assets/svg/events.svg';
-import excelLogo from '../../assets/svg/excelLogo.svg';
+import excelLogo from '../../assets/png/excelLogo.png';
 
 const Bottombar = () => {
   return (
-    <div className="bottombar-container">
+    <div className="bottombarContainer">
       <NavLink to="/schedule">
         <img alt="Schedule" src={schedule} height="20px" width="20px" />
       </NavLink>
       <NavLink to="/competitions">
         <img alt="Competitions" src={competitions} height="20px" width="20px" />
       </NavLink>
-      <NavLink to="/">
-        <img alt="Home" src={excelLogo} height="60px" width="60px" style={{marginTop: '-24px'}} />
+      <NavLink to="/" className="bottombarHomeContainer">
+        <img alt="Home" src={excelLogo} className="bottombarHomeIcon"/>
       </NavLink>
       <NavLink to="/events">
         <img alt="Events" src={events} height="20px" width="20px" />
