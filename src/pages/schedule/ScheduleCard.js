a361695e518908a772cb453dac81f3f160cb98f7
Maskedman99 <maskedman1024@gmail.com>
2020-11-30 17:34:36 +0530

    add animation to card

--- src/pages/schedule/ScheduleCard.js
+++ src/pages/schedule/ScheduleCard.js
@@ -1,14 +1,19 @@
 import dayjs from 'dayjs';
+import {useSpring, animated} from 'react-spring';
 
 import './ScheduleCard.scss';
 
 const ScheduleCard = ({event}) => {
+  const props = useSpring({from: {opacity: 0, marginTop: -50}, to: {opacity: 1, marginTop: 0}});
+
   return (
-    <div className="scheduleCardContainer">
-      <img className="scheduleCardIcon" src={event.icon} alt={event.name} />
-      <div className="scheduleCardTitle">{event.name}</div>
-      <div>{dayjs(event.datetime).format('D/M/YYYY h:mm a')}</div>
-    </div>
+    <animated.div style={props}>
+      <div className="scheduleCardContainer">
+        <img className="scheduleCardIcon" src={event.icon} alt={event.name} />
+        <div className="scheduleCardTitle">{event.name}</div>
+        <div>{dayjs(event.datetime).format('D/M/YYYY h:mm a')}</div>
+      </div>
+    </animated.div>
   );
 };
 
