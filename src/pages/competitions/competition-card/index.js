52fc40423cb25280eee0d096ed47dbb72e07bbd8
Nikhil713 <nikhilbabu213@gmail.com>
2020-12-15 12:09:38 +0530

    remove axios warning

--- src/pages/competitions/competition-card/index.js
+++ src/pages/competitions/competition-card/index.js
@@ -1,7 +1,7 @@
 import {Component} from 'react';
 import Popup from 'reactjs-popup';
 import {Spring, config} from 'react-spring/renderprops';
-import axios from 'axios';
+// import axios from 'axios';
 
 import './index.scss';
 
