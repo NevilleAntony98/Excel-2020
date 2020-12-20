051482e9256ff6abe7913f865675dad4c2e1c29c
Neville <25197458+NevilleAntony98@users.noreply.github.com>
2020-12-20 12:59:21 +0530

    App: Handle authentication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import {Switch, Route, Redirect} from 'react-router-dom';
+import Iframe from 'react-iframe'
+
+import AuthHelper from './utils/authHelper'
 
 import Navbar from './components/Navbar';
-
 import Home from './pages/home';
 import Events from './pages/events';
 import Competitions from './pages/competitions';
@@ -9,12 +12,34 @@ import Schedule from './pages/schedule';
 import Contacts from './pages/contacts';
 
 import './App.scss';
+import NavProfileIcon from './components/NavProfileIcon';
 
 const App = () => {
+  const setIsLoggedIn = useState(false)[1];
+
+  function receiveMessage(event) {
+    if (typeof event.data == 'string' && event.data.length !== 0)
+      AuthHelper.asyncSetRefreshToken(event.data)
+        .then(res => {
+          if (res != null)
+            setIsLoggedIn(true)
+        })
+    else if (event.data == null)
+      AuthHelper.clearAllTokens()
+  }
+
+  window.addEventListener("message", receiveMessage);
+
   return (
     <div className="app-container">
       <Navbar />
+      <div style={{display: 'none'}}>
+        <Iframe src="https://staging.accounts.excelmec.org/auth/authorize" />
+      </div>
       <div className="nav-shift">
+      <div className="mobile-profile-icon">
+        <NavProfileIcon isMobile={true} />
+      </div>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/competitions" component={Competitions} />
